8566bbd574200834af2652d884541a8b9829de44
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-07-22 23:40:02 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,82 +2,109 @@ import requests
 import sqlite3
 from tkinter import *
 from tkinter import messagebox
+import json
 
-def center_window(width=300, height=200):
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+class BookFinder:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Book Finder App")
+        self.center_window(600, 400)
+        self.favorites_db = "favorites.db"
+        self.cache = {}
 
-    x = (screen_width/2) - (width/2)
-    y = (screen_height/2) - (height/2)
-    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+        Label(self.root, text="Enter an ISBN:").pack()
 
-def validate_isbn(isbn):
-    return isbn.isdigit() and len(isbn) in [10, 13]
+        self.isbn_var = StringVar()
+        self.entry = Entry(self.root, width=50, textvariable=self.isbn_var)
+        self.entry.pack()
 
-def search_books():
-    query = entry.get()
-    listbox.delete(0, END)
-    try:
-        response = requests.get(f"https://openlibrary.org/api/books?bibkeys=ISBN:{query}&format=json&jscmd=data")
-        response.raise_for_status()
+        self.isbn_length_var = StringVar(value="10")
+        Spinbox(self.root, from_=10, to=13, textvariable=self.isbn_length_var).pack()
 
-        books = response.json()
+        Button(self.root, text="Search", command=self.search_books).pack()
+        Button(self.root, text="Add to Favorites", command=self.add_to_favorites).pack()
+        Button(self.root, text="Show Favorites", command=self.show_favorites).pack()
+
+        self.scrollbar = Scrollbar(self.root)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.listbox = Listbox(self.root, width=75, height=20, yscrollcommand=self.scrollbar.set)
+        self.listbox.pack()
+
+        self.scrollbar.config(command=self.listbox.yview)
+
+    def center_window(self, width=300, height=200):
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+
+        x = (screen_width/2) - (width/2)
+        y = (screen_height/2) - (height/2)
+        self.root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+    def validate_isbn(self, isbn):
+        return isbn.isdigit() and len(isbn) == int(self.isbn_length_var.get())
+
+    def search_books(self):
+        query = self.isbn_var.get()
+        if not self.validate_isbn(query):
+            messagebox.showerror("Invalid ISBN", "Please enter a valid ISBN.")
+            return
+        self.listbox.delete(0, END)
+        if query in self.cache:
+            self.display_books(self.cache[query])
+        else:
+            try:
+                response = requests.get(f"https://openlibrary.org/api/books?bibkeys=ISBN:{query}&format=json&jscmd=data")
+                response.raise_for_status()
+
+                books = response.json()
+                self.cache[query] = books
+                self.display_books(books)
+            except requests.HTTPError as http_err:
+                messagebox.showerror("HTTP Error", f"HTTP error occurred: {http_err}")
+            except requests.RequestException as req_err:
+                messagebox.showerror("Request Error", f"Other error occurred: {req_err}")
+            except Exception as e:
+                messagebox.showerror("Error", f"An unexpected error occurred: {e}")
+
+    def display_books(self, books):
         for book in books.values():
-            listbox.insert(END, f'Title: {book["title"]}')
-            listbox.insert(END, f'Author: {book["authors"][0]["name"]}')
-            listbox.insert(END, '')
-    except requests.HTTPError as http_err:
-        messagebox.showerror("HTTP Error", f"HTTP error occurred: {http_err}")
-    except requests.RequestException as req_err:
-        messagebox.showerror("Request Error", f"Other error occurred: {req_err}")
-    except Exception as e:
-        messagebox.showerror("Error", f"An unexpected error occurred: {e}")
+            self.listbox.insert(END, f'Title: {book["title"]}')
+            self.listbox.insert(END, f'Author: {book["authors"][0]["name"]}')
+            self.listbox.insert(END, '')
 
-def add_to_favorites():
-    selected = listbox.curselection()
-    if not selected:
-        return
-    title = listbox.get(selected[0])
-    author = listbox.get(selected[0]+1)
-    conn = sqlite3.connect('favorites.db')
-    c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS favorites (title TEXT, author TEXT)")
-    c.execute("INSERT INTO favorites VALUES (?,?)", (title[7:], author[8:]))
-    conn.commit()
-    conn.close()
-    messagebox.showinfo("Success", "Book added to favorites")
+    def add_to_favorites(self):
+        selected = self.listbox.curselection()
+        if not selected:
+            return
+        title = self.listbox.get(selected[0])
+        author = self.listbox.get(selected[0]+1)
+        try:
+            with sqlite3.connect(self.favorites_db) as conn:
+                c = conn.cursor()
+                c.execute("CREATE TABLE IF NOT EXISTS favorites (title TEXT, author TEXT)")
+                c.execute("INSERT INTO favorites VALUES (?,?)", (title[7:], author[8:]))
+        except sqlite3.Error as e:
+            messagebox.showerror("Database Error", f"An error occurred with the database: {e}")
+        else:
+            messagebox.showinfo("Success", "Book added to favorites")
 
-def show_favorites():
-    listbox.delete(0, END)
-    conn = sqlite3.connect('favorites.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM favorites")
-    rows = c.fetchall()
-    conn.close()
-    for row in rows:
-        listbox.insert(END, f'Title: {row[0]}')
-        listbox.insert(END, f'Author: {row[1]}')
-        listbox.insert(END, '')
+    def show_favorites(self):
+        self.listbox.delete(0, END)
+        try:
+            with sqlite3.connect(self.favorites_db) as conn:
+                c = conn.cursor()
+                c.execute("SELECT * FROM favorites")
+                rows = c.fetchall()
+        except sqlite3.Error as e:
+            messagebox.showerror("Database Error", f"An error occurred with the database: {e}")
+        else:
+            for row in rows:
+                self.listbox.insert(END, f'Title: {row[0]}')
+                self.listbox.insert(END, f'Author: {row[1]}')
+                self.listbox.insert(END, '')
 
-root = Tk()
-root.title("Book Finder App")
-center_window(600, 400)
-
-Label(root, text="Enter an ISBN:").pack()
-
-entry = Entry(root, width=50)
-entry.pack()
-
-Button(root, text="Search", command=search_books).pack()
-Button(root, text="Add to Favorites", command=add_to_favorites).pack()
-Button(root, text="Show Favorites", command=show_favorites).pack()
-
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=Y)
-
-listbox = Listbox(root, width=75, height=20, yscrollcommand=scrollbar.set)
-listbox.pack()
-
-scrollbar.config(command=listbox.yview)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    app = BookFinder(root)
+    root.mainloop()
